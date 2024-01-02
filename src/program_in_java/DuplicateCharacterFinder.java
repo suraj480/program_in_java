17a3305bae28874774a910d8bba27aad11021d87
suraj480 <surajmaurya991@gmail.com>
2024-01-03 00:07:15 +0530

    duplicate character

--- src/program_in_java/DuplicateCharacterFinder.java
+++ src/program_in_java/DuplicateCharacterFinder.java
@@ -1,5 +1,21 @@
 package program_in_java;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DuplicateCharacterFinder {
-
+	public static void main(String[] args) {
+		String inputString = "programming";
+		Map<Character, Integer> charCountMap = new HashMap<>();
+		// Iterate through each character in the string
+		for (char c : inputString.toCharArray()) {
+			System.out.println(charCountMap);
+			// Check if the character is already in the map
+			if (charCountMap.containsKey(c)) {
+				System.out.println("Duplicate Character: " + c);
+			} else {
+				// If the character is not in the map, add it with count 1
+				charCountMap.put(c, 1);
+			}
+		}
+	}
 }
